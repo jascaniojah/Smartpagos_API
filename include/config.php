edac522b8ca41925a8daaf4502d6fc1b0b9a521e
Felix David Lugo <felixdlugo@gmail.com>
2014-10-13 00:50:34 -0430

    Anadidos Response Codes a Config.php

--- include/config.php
+++ include/config.php
@@ -6,3 +6,16 @@ define("DB_HOST", "mysql14.000webhost.com");
 define("DB_USER", "a9416723_smartpa");
 define("DB_PASSWORD", "smartpagos.1");
 define("DB_DATABASE", "a9416723_smartp");
+
+
+/* Response Code Variables*/
+define("SUCCESS","000");
+define("NO_SERVICE","100");
+define("INV_IMEI","101");
+define("INV_USER","102");
+define("INV_PSW","103");
+define("INV_ENC","104");
+define("INV_NUMBER","105");
+define("DB_ERROR","996");
+
+?>
